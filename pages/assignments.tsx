43617a771ab477c2be2fe8dff1af1a3b5fb67ea9
FroukjeTemme <66830512+FroukjeTemme@users.noreply.github.com>
2025-05-02 22:22:32 +0200

    pages basic
    pages setup just basic to see how it works

---
+++ pages/assignments.tsx
@@ -0,0 +1,14 @@
+// pages/index.tsx
+import React from "react";
+
+export default function Home() {
+  return (
+    <>
+      <h1 className="text-4xl font-bold mb-4">Welcome to Grading Assistant</h1>
+      <p className="text-gray-600">
+        test assignments
+      </p>
+    </>
+  );
+}
+
---
+++ pages/grading.tsx
@@ -0,0 +1,13 @@
+// pages/index.tsx
+import React from "react";
+
+export default function Home() {
+  return (
+    <>
+      <h1 className="text-4xl font-bold mb-4">Welcome to Grading Assistant</h1>
+      <p className="text-gray-600">
+        test grading
+      </p>
+    </>
+  );
+}
